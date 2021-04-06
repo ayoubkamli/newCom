f4761709d779165245ee701b609a7683de4704fc
Ayyoub Kamli <akamli@e1r10p1.1337.ma>
2021-04-06 15:30:55 +0100

    add small x bonus

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const checkEquation = require("./checkEquation");
 let value = process.argv.slice(2);
 let equation = value[0];
 
-const main = (eq) => {
+const main = (e) => {
   // console.log(eq);
-  checkEquation(eq);
+  eq = e.replace(/ /g, "");
+  eq = eq.toUpperCase();
+  if (eq != "") {
+    checkEquation(eq);
+  } else {
+    console.log("Syntax Error: Please enter a valide form.");
+  }
 };
 
 main(equation);
